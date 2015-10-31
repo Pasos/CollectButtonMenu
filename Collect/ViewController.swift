3772683e5a09ae13e4c53b16458032c537c63244
Pasos <pasosu2005@yahoo.co.jp>
2015-10-31 18:49:32 +0900

    Initial Commit

---
+++ Collect/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Collect
+//
+//  Created by パソス on 2015/10/31.
+//  Copyright © 2015年 パソス. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
